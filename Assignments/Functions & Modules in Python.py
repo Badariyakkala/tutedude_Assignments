90a66bb7915bed0ff06d677f5cca2b0087404c16
Badari <yakkalabadari@outlook.com>
2025-05-05 22:42:42 +0530

    Updated Assignment2

--- Assignments/Functions & Modules in Python.py
+++ Assignments/Functions & Modules in Python.py
@@ -2,12 +2,18 @@
 from math import factorial
 
 def GetFactVal(n):
-    return factorial(n)
+    b = 1
+    c = 1
+    while b < n:
+        b = b + 1
+        c = b * c
+    return c
 
 a=int(input("Enter a number: "))
 result=GetFactVal(5)
-print('Task1 \n Factorial of',a,'is:',  result)
-print('Task2 \n' )
+
+print('Task1: \n Factorial of',a,'is:',  result,'\n')
+print('Task2:' )
 
 ##Using the Math Module for Calculations
 from math import *
---
+++ Practice/Test.py
@@ -0,0 +1,9 @@
+n=5
+b=1
+c=1
+while b<n:
+   #print('start',b)
+    b = b + 1
+    c = b * c
+
+print(c)
